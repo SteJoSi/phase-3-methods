14dbdb2222a4e75e58e8bad1e66baeaf75c37cb9
Stephanie Sison <sjsison27@icloud.com>
2022-11-03 20:02:25 -0700

    commit tree 8a0a383e0d72112c579a5b92d557e29089117ae1

--- methods.rb
+++ methods.rb
@@ -1,1 +1,26 @@
 # Your code here!
+
+#this method will console.log "Hello, programmer!"
+def greet_programmer
+    puts "Hello, programmer!"
+end
+
+# this method takes in the argument name and logs it in the phrase by interpolation
+def greet(name)
+    puts "Hello, #{name}!"
+end
+
+# this method takes the argument of name with a default value of programmer if no name is given and logs it in the phrase by interpolation
+def greet_with_default(name = "programmer")
+    puts "Hello, #{name}!"
+end
+
+# this method takes 2 numbers as arguments and returns the sum of both numbers given
+def add(num1, num2)
+    return num1 + num2
+end
+
+def halve(num)
+    return nil unless num.class == Integer
+    num.div(2)
+end
